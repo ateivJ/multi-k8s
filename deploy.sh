543c91b393a4e855c3a19c81076f31061ab72d02
ateivJ <jain.active@gmail.com>
2019-04-23 07:39:00 -0700

    ateivJ to ateivj in docker.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-dokcer build -t ateivJ/multi-client:latest -t ateivJ/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ateivJ/multi-server:latest -t ateivJ/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ateivJ/multi-worker:latest -t ateivJ/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push ateivJ/multi-client:latest
-docker push ateivJ/multi-server:latest
-docker push ateivJ/multi-worker:latest
-docker push ateivJ/multi-client:$SHA
-docker push ateivJ/multi-server:$SHA
-docker push ateivJ/multi-worker:$SHA
+dokcer build -t ateivj/multi-client:latest -t ateivj/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ateivj/multi-server:latest -t ateivj/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ateivj/multi-worker:latest -t ateivj/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push ateivj/multi-client:latest
+docker push ateivj/multi-server:latest
+docker push ateivj/multi-worker:latest
+docker push ateivj/multi-client:$SHA
+docker push ateivj/multi-server:$SHA
+docker push ateivj/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=ateivJ/multi-server:$SHA
-kubectl set image deployments/client-deployment client=ateivJ/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=ateivJ/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=ateivj/multi-server:$SHA
+kubectl set image deployments/client-deployment client=ateivj/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=ateivj/multi-worker:$SHA
